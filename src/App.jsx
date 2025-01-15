d456a2dc8f2ab0b9c1657397b9556074c281f275
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-01-15 21:22:38 +0300

    app finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   
 
   return (
-    <div className=" m-0 bg-gradient-to-br from-zinc-700 to-zinc-900 h-screen w-screen">
+    <div className=" m-0 bg-gradient-to-br from-zinc-700 to-zinc-900 py-10 h-screen overflow-auto w-screen">
       <h1 className="text-center mx-auto pt-10 bg-gradient-to-r from-slate-50 to-slate-100 bg-clip-text text-transparent font-normal text-4xl">Weather Check</h1>
       <Weather />
     </div>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -28,7 +28,9 @@ const Weather = () => {
             console.log(error)
         }
         
-    }   
+    }
+
+    
 
     function handleInput(e) {
         setCity(e.target.value)
@@ -42,39 +44,39 @@ const Weather = () => {
 
   return (
     <div>
-      <div className="w-2/4 mx-auto mt-10">
-        <input value={city} onChange={handleInput} className="w-full bg-transparent placeholder:text-slate-400 placeholder:font-light text-white text-sm border border-slate-200 rounded-md px-3 py-3 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Check weather..."/>
+      <div className="w-2/4 xl:w-[650px] mx-auto mt-10">
+        <input value={city} onChange={handleInput} className="w-full xl:py-5 mx-auto bg-transparent placeholder:text-slate-400 placeholder:font-light text-white text-sm border border-slate-200 rounded-md px-3 py-3 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Check weather..."/>
       </div>
 
-      <div className='w-2/5 mx-auto mt-8'>
-        <button onClick={handleSubmit} className='w-full px-3 py-3 bg-gradient-to-r from-gray-200 to-slate-400 text-black font-normal rounded-md hover:opacity-80 transition-opacity duration-300'>Get weather</button>
+      <div className='w-2/5 xl:w-[350px] mx-auto mt-8'>
+        <button onClick={handleSubmit} className='w-full mx-auto px-3 py-3 xl:py-5 bg-gradient-to-r from-gray-200 to-slate-400 text-black font-normal rounded-md hover:opacity-80 transition-opacity duration-300'>Get weather</button>
       </div>
 
       {weather && (
     <section className='weather-container px-4 py-4 mt-20 flex flex-col items-center justify-center gap-12'>
 
         
-          <div className='basic-city-info flex flex-row w-[289px] px-6 py-2 rounded-2xl items-center justify-between bg-gradient-to-br from-zinc-800 hover:opacity-80 transition-opacity duration-300 to-zinc-900 text-white'>
+          <div className='basic-city-info flex flex-row w-[289px] lg:w-[400px] 2xl:w-[500px] 2xl:py-4 2xl:gap-32 px-6 py-2 rounded-2xl items-center lg:justify-center lg:gap-16 justify-between bg-gradient-to-br from-zinc-800 hover:opacity-80 transition-opacity duration-300 to-zinc-900 text-white'>
         <div className='flex flex-col gap-4 p-2'>
-            <h2 className='text-2xl'>{weather.name}</h2>
+            <h2 className='text-2xl lg:text-3xl'>{weather.name}</h2>
             <p className='text-slate-300'>{time} • {day}</p>
-            <h1 className='text-4xl'>{Math.floor(weather.main.temp)}°</h1>
+            <h1 className='text-4xl lg:text-5xl'>{Math.floor(weather.main.temp)}°</h1>
         </div>
-        <img width={80} src={weather.weather[0].description === "few clouds" ? "../../public/sun-cloudy.svg" : `../../public/${weather.weather[0].main}.svg`} alt="" />
+        <img width={80} className='lg:w-[120px] 2xl:w-[150px]' src={weather.weather[0].description === "few clouds" ? "../../public/sun-cloudy.svg" : `../../public/${weather.weather[0].main}.svg`} alt="" />
           </div>
 
-          <div className='hourly-weather-info w-11/12 bg-gradient-to-br from-slate-800 rounded-2xl to-slate-900 flex flex-row gap-4 items-center justify-center text-white'>
-            {forecast.length > 0 && (
+          <div className='hourly-weather-info hover:opacity-80 transition-opacity duration-300 w-11/12 bg-gradient-to-br from-slate-800 rounded-2xl to-slate-900 flex flex-row gap-4 items-center justify-center text-white'>
+            {forecast && (
 
-            <div className='forecast'>
-              <div className='forecast-days flex flex-row gap-12 py-8 px-4 items-center justify-center'>
+            <div className='forecast transition-opacity hover:opacity-80 duration-300'>
+              <div className='forecast- flex flex-row gap-10 py-8 px-8 items-center justify-between md:gap-14 lg:gap-20 xl:gap-28 2xl:gap-48'>
                 {forecast.map((item, index) => (
-                  <div key={index} className='forecast-day flex flex-col gap-6 items-center justify-center'>
-                    <p >{new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                  <div key={index} className='forecast-day flex flex-col gap-8 items-center text-center justify-center'>
+                    <p className='2xl:text-2xl'>{new Date(item.dt * 1000).toLocaleDateString("en-US", {
                 weekday: "short",
               })}</p>
-                    <img width={80} src={item.weather[0].description === "few clouds" ? "../../public/sun-cloudy.svg" : `../../public/${item.weather[0].main}.svg`} alt="" />
-                    <p>{Math.floor(item.main.temp)}°</p>
+                    <img className='lg:w-[100px] 2xl:w-[120px]' src={item.weather[0].description === "few clouds" ? "../../public/sun-cloudy.svg" : `../../public/${item.weather[0].main}.svg`} alt="" />
+                    <p className='text-2xl 2xl:text-4xl'>{Math.floor(item.main.temp)}°</p>
                   </div>
                 ))}
               </div>
